f53ddb4627b063a65968ab60a0a53250d227e57f
Davide Quaranta <d.quaranta8@studenti.unisa.it>
2024-12-20 19:11:16 +0100

    Delete PROGETTO_TSW/Registration directory

--- PROGETTO_TSW/Registration/headerReg.php
+++
@@ -1,4 +0,0 @@
-<div id="header">
-    <img src="/PROGETTO_TSW/Registration/icone/Logo_40x40.png" alt="LOGO" width="40px" heigth="40px">
-    <img  id="iconaUtente" alt="ICONA UTENTE" src="/PROGETTO_TSW/Registration/icone/icona-utente.png" width="40px" heigth="40px">
-</div>
