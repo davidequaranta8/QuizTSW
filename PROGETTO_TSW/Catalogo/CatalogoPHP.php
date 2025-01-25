b826dd159c7a739bafb2830d7c7aff089e57bee3
C1R0nca <roncaciro7@gmail.com>
2025-01-25 15:52:07 +0100

    Add files via upload

---
+++ PROGETTO_TSW/Catalogo/CatalogoPHP.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalogo</title>
+
+    <!-- Link ai file CSS -->
+    <link rel="stylesheet" href="/PROGETTO_TSW/Catalogo/CatalogoCSS.css">
+    <!-- <link rel="stylesheet" href="/PROGETTO_TSW/utility/footerStyle.css"> -->
+
+
+    <link rel="icon" type="image/png" href="/PROGETTO_TSW/utility/icone/Logo_codeQuiz.ico">
+</head>
+<body>
+
+    <header>
+        <span id="navigationBar"><span>Home</span><span>Catalogo</span></span>
+        <img id="iconaUtente" src="/PROGETTO_TSW/homepage/foto/icona-utente.png" alt="ICONA UTENTE">
+        <span id="loginStr">Login</span>
+        <img id="coppa" src="/PROGETTO_TSW/utility/icone/coppa.png" alt="coppa">
+        <img id="logo" src="/PROGETTO_TSW/utility/icone/Logo_codeQuiz.png" alt="logo">
+    </header>
+    <br> <br>
+    <h2>Java</h2> <br>
+
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+
+    <h2>HTML</h2> <br>
+
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+
+    <h2>C</h2> <br>
+
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+
+    <h2>JavaScript</h2> <br>
+
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    
+    <h2>PHP</h2> <br>
+
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+
+    <h2>CSS</h2> <br>
+
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    
+    <h2>Assembly</h2> <br>
+    
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+    <img src="/PROGETTO_TSW/utility/icone/phpCopertinaWide.jpg">
+
+
+    <!-- footer -->
+    <?php include_once($_SERVER['DOCUMENT_ROOT'] . "/PROGETTO_TSW/utility/footerPHP.php") ?>
+    
+
+</body>
+</html>
