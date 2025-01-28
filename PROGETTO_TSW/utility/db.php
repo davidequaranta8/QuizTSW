a0019af05e4548a2b529f33d7390064f3c7b1598
Davide Quaranta <d.quaranta8@studenti.unisa.it>
2025-01-28 17:31:10 +0100

    Delete PROGETTO_TSW/utility directory

--- PROGETTO_TSW/utility/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = 'localhost';
-$port = '5432';
-$dbn = 'Gruppo28';
-$user = 'www';
-$pass = 'Davide1234';
-$connection_string = "host=$host port=$port dbname=$dbn user=$user password=$pass";
-
-?>
